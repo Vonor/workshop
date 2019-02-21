d0296c7912dc3140621d512fb78e79d2ffd1eb60
Michael Melcher <michael.melcher82@gmail.com>
2019-02-21 22:09:24 +0100

    03-simple-service

---
+++ Docker/03-simple-service/index.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const port = process.env.PORT || 3000
+const fs = require('fs');
+
+const requestHandler = (request, response) => {
+  if (request.url === "/stop") {
+    response.end('Node JS Server stopped')
+    process.exit()
+  }
+  if (fs.existsSync('/app/www-root/index.html')) {
+    var content = fs.readFileSync('/app/www-root/index.html', 'utf8');
+    response.end(content)
+  } else {
+    response.end('Hello Node.js Server!')
+  }
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
